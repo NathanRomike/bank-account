4832c22c30ddf3c7c4ba54973ff95682d5b92565
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-02-09 17:50:21 -0800

    Fix deposit and withdraw functions.

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ BankAccount.prototype.updateAmount = function(amount, flag) {
 
 $(document).ready(function() {
   $("form.add-user").submit(function(event) {
-    // debugger;
     event.preventDefault();
     var inputtedUserName = $(this).find("input#new-name").val();
     var inputtedinitial = parseInt($(this).find("input#initial-deposit").val());
@@ -21,19 +20,22 @@ $(document).ready(function() {
 
     $("h3#balance").text(newUser.balance);
     $(".add-user").hide();
-
-  });
-
-  $("form.update-amount").submit(function() {
-    var deposit = parseInt($(this).find("input#deposit-amount").val());
-    var withdraw = parseInt($(this).find("input#withdraw-amount").val());
-
-    if (deposit === NaN) {
-      newUser.updateAmount(withdraw, "W");
-    }
+    $(".balance").show();
 
 
-  $("h3#balance").text(newUser.balance);
+    $("form.update-amount").submit(function(event) {
+      event.preventDefault();
+      var deposit = parseInt($(this).find("input#deposit-amount").val());
+      var withdraw = parseInt($(this).find("input#withdraw-amount").val());
 
+      if (deposit = "NaN") {
+        newUser.updateAmount(withdraw, "W");
+      } else {
+        newUser.updateAmount(deposit, "D");
+      }
+
+      $("h3#balance").text(newUser.balance);
+
+    });
   });
 });
